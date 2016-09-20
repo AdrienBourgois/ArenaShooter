7aa3c7d425705f8db013b4d740f3994abf3d9f76
Unknown <adrien.bourgois@sfr.fr>
2016-09-20 15:24:39 +0200

    Simple player movement - BaseBullet

---
+++ Assets/Scripts/BaseBullet.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BaseBullet : MonoBehaviour {
+
+    public float speed = 20f;
+    Vector3 direction;
+	
+    public void shoot(Vector3 from, Vector3 to)
+    {
+        transform.position = from;
+        transform.LookAt(to);
+        direction = to;
+    }
+
+	void FixedUpdate()
+    {
+        Vector3 position = transform.position;
+        position += direction.normalized * speed * Time.deltaTime;
+        transform.position = position;
+	}
+
+    void destroy()
+    {
+        Destroy(gameObject);
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,16 +3,42 @@ using System.Collections;
 
 public class Player : MonoBehaviour {
 
-	void Start () {
-	
-	}
+    public float playerSpeed = 2f;
+    public Vector3 deltaCamera = new Vector3(0,3,-3);
+    Camera cam = null;
+    public float bulletCooldowm = 0.7f;
+    float nextBulletColldown = 0f;
+
+    void Awake()
+    {
+        nextBulletColldown = bulletCooldowm;
+        cam = GetComponentInChildren<Camera>();
+        cam.transform.LookAt(transform);
+    }
 	
 	void Update () {
-        if (Input.GetButton("Horizontal"))
+        Vector3 position = transform.position;
+        cam.transform.position = position + deltaCamera;
+
+        if (nextBulletColldown > 0)
+            nextBulletColldown -= Time.deltaTime;
+        else if (nextBulletColldown < 0)
+            nextBulletColldown = 0;
+
+        if (Input.GetButton("Horizontal") || Input.GetButton("Vertical"))
         {
-            Vector3 position = transform.position;
-            position.x += Input.GetAxis("Horizontal") * Time.deltaTime;
+            position.x += Input.GetAxis("Horizontal") * Time.deltaTime * playerSpeed;
+            position.z += Input.GetAxis("Vertical") * Time.deltaTime * playerSpeed;
             transform.position = position;
         }
-	}
+
+        if (Input.GetButton("Fire1") && nextBulletColldown == 0)
+        {
+            BaseBullet bulletPrefab = Resources.Load<BaseBullet>("Prefabs/BaseBullet");
+            BaseBullet bullet = Instantiate(bulletPrefab);
+            bullet.shoot(transform.position, new Vector3(20, 1, 20));
+            nextBulletColldown = bulletCooldowm;
+        }
+
+    }
 }
