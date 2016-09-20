70fb6a8836eb588762520cfd97273015704db6f9
Unknown <adrien.bourgois@sfr.fr>
2016-09-20 14:30:15 +0200

    Start project - Simple scene

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	void Start () {
+	
+	}
+	
+	void Update () {
+        if (Input.GetButton("Horizontal"))
+        {
+            Vector3 position = transform.position;
+            position.x += Input.GetAxis("Horizontal") * Time.deltaTime;
+            transform.position = position;
+        }
+	}
+}
