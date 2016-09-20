a19d12d8fd478480438077184b1314bceb0d88d0
Adrien <a.bourgois@student.isartdigital.com>
2016-09-20 17:32:17 +0200

    Collision between Enemies and Bullets (need improvements)

--- Assets/Scripts/BaseEnemy.cs
+++ Assets/Scripts/BaseEnemy.cs
@@ -5,6 +5,7 @@ public class BaseEnemy : MonoBehaviour {
 
     public int life = 1;
     public float enemySpeed = 2f;
+    public float gravity = 20f;
     Player player = null;
     CharacterController enemyController = null;
 
@@ -14,13 +15,26 @@ public class BaseEnemy : MonoBehaviour {
         player = Player.Instance;
     }
 
-    public void attackPlayer()
+    public void Hit()
     {
-        
+        --life;
+        if (life <= 0)
+            Destroy(gameObject);
     }
 
-	void Update () {
+    void OnCollisionEnter(Collision collision)
+    {
+        GameObject go = collision.gameObject;
+        if (go.layer == LayerMask.NameToLayer("Bullet"))
+        {
+            Hit();
+            Destroy(go);
+        }
+    }
+
+    void Update () {
         Vector3 direction = player.transform.position - transform.position;
+        direction.y -= gravity * Time.deltaTime;
         enemyController.SimpleMove(direction.normalized * enemySpeed);
 	}
 }
